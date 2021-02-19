5ddaa3ab5e091f129cd7c12d7bf74a56e3090158
ihValery <ihValery@gmail.com>
2021-02-19 20:09:40 +0300

    START Good Lucky )))

--- Lesson14/ViewController.swift
+++ Lesson14/ViewController.swift
@@ -1,17 +1,10 @@
-//
-//  ViewController.swift
-//  Lesson14
-//
-//  Created by Валерий Игнатьев on 19.02.2021.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+   
     }
 
 
