99504d4ef881319cb58b44bb5500cdc09fddb0fc
ihValery <ihValery@gmail.com>
2021-02-19 19:26:27 +0300

    Initial Commit

---
+++ Lesson14/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson14
+//
+//  Created by Валерий Игнатьев on 19.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
